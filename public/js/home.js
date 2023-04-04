b1dbd279261d7ce6d1e4b26dffae4f15536ade31
Lauren Garcia <lauren_garcia@dlsu.edu.ph>
2023-04-04 15:57:34 +0800

    clear function

---
+++ public/js/home.js
@@ -0,0 +1,19 @@
+function clearUpdateInputs() {
+    document.getElementById("movieId").value = '';
+    document.getElementById("director").value = '';
+    document.getElementById("movietitle").value = '';
+    document.getElementById("actor1").value = '';
+    document.getElementById("actor2").value = '';
+    document.getElementById("year").value = '';
+    document.getElementById("genre").value = '';
+}
+
+function clearInsertInputs() {
+    document.getElementById("director").value = '';
+    document.getElementById("movietitle").value = '';
+    document.getElementById("actor1").value = '';
+    document.getElementById("actor2").value = '';
+    document.getElementById("year").value = '';
+    document.getElementById("genre").value = '';
+}
+
