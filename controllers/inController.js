76ac4f722d124adf5a706488a29555f66f616534
Annika Capada <80928976+AnnikaCap@users.noreply.github.com>
2023-04-17 01:37:30 +0800

    Update inController.js

--- controllers/inController.js
+++ controllers/inController.js
@@ -302,14 +302,14 @@ const inController = {
             console.log("autocommit = 0")
             await node1Connection.query("START TRANSACTION;")
             console.log("transaction started")
-            await node1Connection.query("LOCK TABLES central write, logs WRITE;;")
+            await node1Connection.query("LOCK TABLES central write, logs WRITE;")
             console.log("tables are locked")
 
             //logs
             console.log("Start log inserted to central logs")
             var sqlEntryLog = `INSERT INTO central (title, year, genre, director, actor1, actor2) VALUES ('${title}',${year},'${genre}','${director}','${actor1}','${actor2}')`;
             console.log("after log")
-            console.log(err)
+
             //update logs
             var sqlEntryFill = 'INSERT INTO logs (operation, sql_statement, node_id, status) VALUES (?,?,?,?)';
             datalist = node1Connection.query(sqlEntryFill, ['INSERT', sqlEntryLog, 1, 'start'])
@@ -408,29 +408,6 @@ const inController = {
                 if (node3Connection != null) {
                     node3Connection.end()
                 }
-                /*
-                if (nodeLogsConnection != null) {
-                    node3Connection.end()
-                }*/
-
-                // update logs status = terminated, since nag error sa lahat
-                /*
-                try {
-                    nodeLogsConnection = await mysql.createConnection(config.nodeLogsConn)
-
-                    await nodeLogsConnection.query("INSERT INTO `node3_logs` (`operation`, `name`, `year`, `rank`, `status`, `dest`) VALUES ('insert', '" + movieName + "'," + movieYear + "," + movieRank + ", 'terminated', 'node3');")
-                    console.log("Logs in node3_logs terminated")
-
-                    await nodeLogsConnection.query("INSERT INTO `node1_2_logs` (`operation`, `name`, `year`, `rank`, `status`, `dest`) VALUES ('insert', '" + movieName + "'," + movieYear + "," + movieRank + ", 'terminated', 'node1');")
-                    console.log("Logs in node1_logs terminated")
-
-                } catch(err) {
-                    console.log(err)
-                    if(nodeLogsConnection != null) {
-                        nodeLogsConnection.end()
-                    }
-                }
-                */
             }
                if (flag3) {
                 try{
