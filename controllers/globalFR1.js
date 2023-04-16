b61c65d8d69746b0395ed37223965eddbc3d96cc
Lauren Garcia <lauren_garcia@dlsu.edu.ph>
2023-04-16 18:20:17 +0800

    node 3 case 1

--- controllers/globalFR1.js
+++ controllers/globalFR1.js
@@ -84,7 +84,6 @@ const globalFR1Controller = {
                 sqlEntryFill = 'SELECT id FROM node2 ORDER BY id DESC LIMIT 1';
                 let selectlist = node2Connection.query(sqlEntryFill)
 
-                
                 selectlist.then(function(result) {
                     console.log('-------')
                     console.log(result)
@@ -98,12 +97,9 @@ const globalFR1Controller = {
                     console.log("?")
                     console.log(recentId)
                 }) 
-                console.log("asdfasdfkjasd")
-
                 //perform insert
                 sqlEntryFill = 'INSERT INTO node2 (title, year, genre, director, actor1, actor2) VALUES (?,?,?,?,?,?)';
                 datalist = node2Connection.query(sqlEntryFill, [title, year, genre, director, actor1,actor2])
-                console.log("WHERES THE ID")
 
                 datalist.then(function(result) {
                     console.log(result)
@@ -123,6 +119,71 @@ const globalFR1Controller = {
                 node2Connection.end()
                 console.log('inserted into node 2')
             }
+        }
+        else if (year >= 1980)  {
+            try {
+                node1Connection = await mysql.createConnection(config.node1conn)
+                console.log('connected to central node');
+
+                node1Connection.destroy();
+                console.log('Node 1 destroyed')
+                (await node1Connection).ping(function (err) {
+                    if (err) {
+                        console.log('Central node failed!')
+                    } 
+                    
+                })
+            }
+            catch (err){
+                node3Connection = await mysql.createConnection(config.node3conn)
+                console.log('connected to node 3!');
+    
+                await node3Connection.query(setIsolationLevel)
+                console.log("Isolation level is set to: " + isolationLevelDefault)
+
+                await node3Connection.query("set autocommit = 0;");
+                await node3Connection.query("START TRANSACTION;");
+                await node3Connection.query("LOCK TABLES node2 WRITE, logs WRITE;");
+                
+                //table for reference: id, operation, sql_statement, node_id, status
+                //await node2Connection.query("INSERT INTO `logs` (id, operation, sql_statement, node_id, status) VALUES ('insert', '" + movieName + "'," + movieYear + "," + movieRank + ", 'start', 'node1');")
+                console.log("Start log inserted to node 3 logs")
+                var sqlEntryLog = `INSERT INTO node3 (title, year, genre, director, actor1, actor2) VALUES ('${title}',${year},'${genre}','${director}','${actor1}','${actor2}')`;
+
+                //update logs
+                var sqlEntryFill = 'INSERT INTO logs (operation, sql_statement, node_id, status) VALUES (?,?,?,?)';
+                let datalist = node3Connection.query(sqlEntryFill, ['INSERT', sqlEntryLog, 1, 'start'])
+
+                datalist.then(function(result) {
+                    console.log(result)
+                    logId = result[0].insertId
+                    console.log("logid:")
+                    console.log(logId)
+                }) 
+
+                //perform insert
+                sqlEntryFill = 'INSERT INTO node3 (title, year, genre, director, actor1, actor2) VALUES (?,?,?,?,?,?)';
+                datalist = node3Connection.query(sqlEntryFill, [title, year, genre, director, actor1,actor2])
+
+                datalist.then(function(result) {
+                    console.log(result)
+                    console.log(result[0].insertId) // "Some User token"
+                }) 
+                
+                //set log as write
+                //await nodeLogsConnection.query('UPDATE `logs` SET `status` = ? WHERE `name` = ? AND `dest` = ?;', ['committing', title, 'central'])
+
+                await node3Connection.query('UPDATE `logs` SET `status` = ? WHERE `id` = ?;', ['committing', logId]);
+                await node3Connection.query("COMMIT;");
+                //node2Connection.query("UPDATE logs SET status = 'committed' WHERE id = " + [logId]);
+                await node3Connection.query('UPDATE `logs` SET `status` = ? WHERE `id` = ?;', ['committed', logId]);
+                await node3Connection.query("UNLOCK TABLES;");
+
+                //update logs
+                node3Connection.end()
+                console.log('inserted into node 3')
+            }
+            
         }
 
         /*
