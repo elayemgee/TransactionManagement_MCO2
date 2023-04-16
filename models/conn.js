6ca94acfd64ebbd84429dd10c11d25716ca3d787
Lauren Garcia <lauren_garcia@dlsu.edu.ph>
2023-04-16 10:00:18 +0800

    Update conn.js

--- models/conn.js
+++ models/conn.js
@@ -6,26 +6,6 @@ const node1conn = {
     database: 'movies'
 }
 
-/*
-const node1conn = {
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'net11142',
-    database: 'movies'
-}
-*/
-
-/*
-const node2conn = {
-    host: '172.16.3.143',
-    port: '3306',
-    user: 'group16',
-    password: '12341234',
-    database: 'movies'
-}
-*/
-
 const node2conn = {
     host: '172.16.3.143',
     port: '3306',
@@ -41,33 +21,5 @@ const node3conn = {
     password: '12341234',
     database: 'movies'
 }
-/*
-const node2conn = {
-    host: "mco2-node2.mysql.database.azure.com",
-    port: "3306",
-    user: "root",
-    password: "Password!",
-    database: "node2_db",
-    connectionLimit: 10
-}
-
-const node3conn = {
-    host: "mc02-node3.mysql.database.azure.com",
-    port: "3306",
-    user: "root",
-    password: "Password!",
-    database: "node3_db",
-    connectionLimit: 10
-}
-
-const nodeLogsConn = {
-    host: "mco2-logs.mysql.database.azure.com",
-    port: "3306",
-    user: "MC02GRP21",
-    password: "Password!",
-    database: "node_logs_db",
-    connectionLimit: 10
-}
-*/
 
 module.exports = { node1conn, node2conn, node3conn }
