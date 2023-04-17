cfd6b0f1f01e425dd4b70454eceeab73312529d5
Lauren Garcia <lauren_garcia@dlsu.edu.ph>
2023-04-17 17:20:05 +0800

    Update globalFR3.js

--- controllers/globalFR3.js
+++ controllers/globalFR3.js
@@ -111,7 +111,7 @@ const globalFR3Controller = {
                 var sqlEntryLog = `UPDATE central SET title = '${title}', year = ${year}, genre = '${genre}', director = '${director}', actor1 = '${actor1}', actor2 = '${actor1}' WHERE id = '${id}'`;
                 //update logs
                 var sqlEntryFill = 'INSERT INTO logs (id, operation, sql_statement, node_id, status) VALUES (?, ?,?,?,?)';
-                let datalist = await node1Connection.query(sqlEntryFill, [logId, 'UPDATE', sqlEntryLog, 2, 'start'])
+                let datalist = node1Connection.query(sqlEntryFill, [logId, 'UPDATE', sqlEntryLog, 2, 'start'])
                 console.log("after start")
                 
 				datalist.then(function(result) {
